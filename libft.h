e5eec012b3452e4298d4b740b33ba2ee525793b1
ccosta-c <ccosta-c@student.42porto.com>
2022-11-08 15:33:06 +0000

    added memmvove to libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:07:09 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/07 17:47:18 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/08 13:58:56 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *s);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
