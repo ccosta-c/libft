343527a209a8db3ec9eaa98b53c11d180e479548
ccosta-c <ccosta-c@student.42porto.com>
2022-11-09 17:45:37 +0000

    created function strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 15:37:00 by ccosta-c          #+#    #+#             */
+/*   Updated: 2022/11/09 17:26:07 by ccosta-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strrchr(const char *s, int c)
+{
+	const char	*original;
+	int	i;
+
+	original = s;
+	i = ft_strlen(s);
+	s = (s + i);
+
+	while (*s != *original && *s != c)
+	{
+		s--;
+	}
+	if (*s == c)
+	{
+		return ((char *)s);
+	}
+	return (0);
+}
