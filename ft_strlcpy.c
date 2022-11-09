e7623808488eb50710b7eac769e172f1919d4239
ccosta-c <ccosta-c@student.42porto.com>
2022-11-09 11:31:41 +0000

    added the function

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,29 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 15:39:23 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/08 15:43:08 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/09 11:30:09 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t strlcpy (char *dst, const char *src, size_t size)
+size_t	ft_strlcpy (char *dst, const char *src, size_t size)
 {
-	
+	size_t	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (*(src + i) && (i < (size - 1)))
+		{
+			*(dst + i) = *(src + i);
+			i++;
+		}
+	}
+	*(dst + i) = '\0';
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
