7f9167093527ce58453d5e2f2dee1a52bf3349bf
ccosta-c <ccosta-c@student.42porto.com>
2022-11-09 15:39:25 +0000

    added 3 functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:07:09 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/09 11:58:22 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/09 15:34:06 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,8 @@ void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *s);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int ft_toupper(int c);
+int ft_tolower(int c);
 
 #endif
