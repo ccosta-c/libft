677ae80e1a9cd297846a344dcad931632e3558b9
ccosta-c <ccosta-c@student.42porto.com>
2022-11-10 14:03:25 +0000

    wrote memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 13:28:00 by ccosta-c          #+#    #+#             */
+/*   Updated: 2022/11/10 13:51:10 by ccosta-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+	char	*str1;
+	char	*str2;
+
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	
+	i = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (((unsigned char)str1[i] == (unsigned char)str2[i]) && (i < (n - 1)))
+	{
+		i++;
+	}
+	return (str1[i] - str2[i]);
+}
