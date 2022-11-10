f88a1d3c1736dd5a3b30f012eb1aeae17c605768
ccosta-c <ccosta-c@student.42porto.com>
2022-11-10 13:26:16 +0000

    added memchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:07:09 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/10 11:43:40 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/10 12:54:57 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void 	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
