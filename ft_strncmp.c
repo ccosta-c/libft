29cdda36f3f309622b1f6a1b8b6d24b02711b29a
ccosta-c <ccosta-c@student.42porto.com>
2022-11-10 13:26:03 +0000

    more efficient strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:32:17 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/10 11:41:18 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/10 13:16:46 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,13 @@ int 	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 	
 	i = 0;
-	while ((!s1[i] && !s2[i]) && i < n)
+	if (n == 0)
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
+		return (0);
+	}
+	while ((!s1[i]) && (s1[i] == s2[i]) && (i < (n - 1)))
+	{
 		i++;
 	}
-	if (i != n)
-	{
-		return (s1[i] - s2[i]);
-	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
