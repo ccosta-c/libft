a57bc8f7bd64382f13062e34e0278a5f0345fff1
ccosta-c <ccosta-c@student.42porto.com>
2022-11-14 19:18:01 +0000

    conversion erros

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 13:28:00 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/14 14:33:22 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/14 19:14:06 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
-	char	*str1;
-	char	*str2;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	str1 = (char *)s1;
-	str2 = (char *)s2;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	i = 0;
 	if (n == 0)
 	{
 		return (0);
 	}
-	while (((unsigned char)str1[i] == (unsigned char)str2[i]) && (i < (n - 1)))
+	while ((str1[i] == str2[i]) && (i < (n - 1)))
 	{
 		i++;
 	}
