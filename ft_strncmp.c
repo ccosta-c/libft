00232babfe979d2dd411da8587735ff783455d6f
ccosta-c <ccosta-c@student.42porto.com>
2022-11-14 19:40:22 +0000

    chnages unsigned char related

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:32:17 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/14 14:36:23 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/14 19:39:36 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	unsigned char *str1;
+	unsigned char *str2;
 
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
 	i = 0;
 	if (n == 0)
 	{
 		return (0);
 	}
-	while ((!s1[i]) && (s1[i] == s2[i]) && (i < (n - 1)))
+	while ((str1[i] != '\0') && (str1[i] == str2[i]) && (i < (n - 1)))
 	{
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (str1[i] - str2[i]);
 }
