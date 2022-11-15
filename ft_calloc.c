bd053c9983ecbdff903ee0bb60d2116a76c249d0
ccosta-c <ccosta-c@student.42porto.com>
2022-11-15 14:48:43 +0000

    created calloc

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 14:39:19 by ccosta-c          #+#    #+#             */
+/*   Updated: 2022/11/14 16:58:47 by ccosta-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+	{
+		return (ptr);
+	}
+	ft_bzero(ptr, (size * nmemb));
+	return (ptr);
+}
