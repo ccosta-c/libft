192eff4335d52b6af653db6a0a34840d7de35704
ccosta-c <ccosta-c@student.42porto.com>
2022-11-16 14:10:36 +0000

    added strtrim and added a list fo the functions

--- libft.h
+++ libft.h
@@ -6,9 +6,37 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:07:09 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/15 19:21:45 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/16 14:07:34 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+bzero - write zeroes to a byte string;
+isalnum - test for any character for which isalpha or isdigit is true;
+isalpha - test for an alphabetic character;
+isdigit - test for a decimal digit character;
+isascii - test for an ASCII character;
+memcpy - copy memory area up to n bytes, not handle overlaps;
+memmove - copy memory area up to n bytes, handles overlaps;
+memset - fills the first n bytes of the memory area pointed to by s with the constant byte c;
+strlen - calculate the length of a string;
+strlcpy - copy string up to n bytes;
+strlcat - concatenate string up to n bytes;
+strtoupper - convert lowercase letter to uppercase;
+strtolower - convert uppercase letter to lowercase;
+strchr - locate character in string;
+strrchr - locate last occurrence of character in string;
+strncmp - compare strings up to n bytes;
+memchr - locate byte in byte string;
+memcmp - compare bytes in memory;
+strnstr - locate a substring in a string up to n bytes;
+atoi - convert ASCII string to integer;
+calloc - allocate memory for an array;
+strdup - duplicate a string;
+substr - allocate and return a substring from the string ’s’, starting at index ’start’ and of maximum size ’len’;
+strjoin - concatenate ’s1’ and ’s2’;
+strtrim - trim characters in the string ’set’ from the beginning and the end of the string ’s1’;
+*/
 
 #ifndef LIBFT_H
 # define LIBFT_H
@@ -23,7 +51,6 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *s);
 void	*ft_memmove(void *dest, const void *src, size_t n);
@@ -42,5 +69,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
