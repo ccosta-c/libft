2134a7ff65223c34a0e43485ae783d792a811bc5
ccosta-c <ccosta-c@student.42porto.com>
2022-11-19 15:10:26 +0000

    wrote lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 15:03:59 by ccosta-c          #+#    #+#             */
+/*   Updated: 2022/11/19 15:09:46 by ccosta-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*current;
+	t_list	*next;
+
+	if(lst == NULL || *lst == NULL || del == NULL)
+	{
+		return ;
+	}
+	current = *lst;
+	while (current != NULL)
+	{
+		next = current->next;
+		(del)(current->content);
+		free(current);
+		current = next;
+	}
+	*lst = NULL;
+}
