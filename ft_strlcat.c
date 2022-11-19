2776193f52b297f28aea1bdabfd694aaa274e81d
ccosta-c <ccosta-c@student.42porto.com>
2022-11-19 14:43:10 +0000

    rewrote strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 11:33:29 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/09 18:03:04 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/19 14:41:01 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,34 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-	size_t	d;
-	size_t	src_len;
-	size_t	dst_len;
+	char			*s;
+	unsigned int	src_len;
+	unsigned int	dst_len;
+	unsigned int	i;
+	unsigned int	sol;
 
+	s = (char *)src;
+	i = 0;
+	if (size == 0)
+	{
+		return (ft_strlen(src));
+	}
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
-	i = 0;
-	d = dst_len;
-	if (dst_len < (size - 1) && size > 0)
+	if (size > dst_len)
 	{
-		while (!(src + i) && dst_len < size - 1)
-		{
-			*(dst + d) = *(src + i);
-			i++;
-			d++;
-		}
-		*(dst + d) = '\0';
+		sol = src_len + dst_len;
 	}
-	if (dst_len >= size)
+	else
 	{
-		dst_len = size;
+		sol = size + src_len;
 	}
-	return (dst_len + src_len);
+	while(s[i] && (dst_len + 1) < size)
+	{
+		dst[dst_len] = s[i];
+		i++;
+		dst_len++;
+	}
+	dst[dst_len] = '\0';
+	return (sol);
 }
