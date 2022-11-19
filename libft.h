7b7ff8898e405a773b6550fa7c1227722e2fd774
ccosta-c <ccosta-c@student.42porto.com>
2022-11-19 15:10:58 +0000

    added to lstdelone and lstclear

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:07:09 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/18 16:40:35 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/19 15:08:47 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,5 +94,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void 	ft_lstclear(t_list **lst, void (*del)(void*));
 
 #endif
