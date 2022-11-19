8efbdc405ffe0d4e48063b904f6cd94832bb9e5f
ccosta-c <ccosta-c@student.42porto.com>
2022-11-19 17:29:21 +0000

    fixed an error

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:07:09 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/19 17:23:08 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/19 17:28:52 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,6 @@ ft_lstmap		- Iterates the list *lst* and applies the function *f* to the
 	content of each node. Creates a new list resulting of the successive 
 	applications of the function *f*.
 */	
-*/
 
 #ifndef LIBFT_H
 # define LIBFT_H
