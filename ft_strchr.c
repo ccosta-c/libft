4debf11740b18e20dba18d34bb035852c578b2e9
ccosta-c <ccosta-c@student.42porto.com>
2022-11-20 12:24:07 +0000

    simplified using the memchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:37:00 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/09 18:19:36 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/20 12:07:03 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,5 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != '\0' && *s != c)
-	{
-		s++;
-	}
-	if (*s == c)
-	{
-		return ((char *)s);
-	}
-	return (0);
+	return (ft_memchr(s, c, ft_strlen(s) + 1));
 }
