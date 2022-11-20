04bf416e306de0cd295b1a1d9f09cc3e0d9fd163
ccosta-c <ccosta-c@student.42porto.com>
2022-11-20 12:23:22 +0000

    improved function by removing useless checks

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: ccosta-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 16:05:27 by ccosta-c          #+#    #+#             */
-/*   Updated: 2022/11/19 16:41:43 by ccosta-c         ###   ########.fr       */
+/*   Updated: 2022/11/20 12:19:29 by ccosta-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,12 @@
 int	ft_lstsize(t_list *lst)
 {
 	int		count;
-	t_list	*ptr;
 
 	count = 0;
-	if (!lst)
-	{
-		return (0);
-	}
-	ptr = lst;
-	while (ptr != 0)
+	while (lst != 0)
 	{
 		count++;
-		ptr = ptr->next;
+		lst = lst->next;
 	}
 	return (count);
 }
